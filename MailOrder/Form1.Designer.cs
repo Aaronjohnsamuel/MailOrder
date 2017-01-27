0f8e54e26582f826417202a745fdbd7ec01ee89d
aaronjohn <aaronsamual@gmail.com>
2017-01-27 00:19:23 -0500

    all completed changes

--- MailOrder/Form1.Designer.cs
+++ MailOrder/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MailOrder
 {
-    partial class Form1
+    partial class MailOrder
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,236 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MailOrder));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblEmployeeName = new System.Windows.Forms.Label();
+            this.lblEmployeeId = new System.Windows.Forms.Label();
+            this.lblHoursWorked = new System.Windows.Forms.Label();
+            this.lblTotalSales = new System.Windows.Forms.Label();
+            this.lblSalesBonus = new System.Windows.Forms.Label();
+            this.txtEmployeeName = new System.Windows.Forms.TextBox();
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.txtHoursWorked = new System.Windows.Forms.TextBox();
+            this.txtTotalSales = new System.Windows.Forms.TextBox();
+            this.txtSalesBonus = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdoFrench = new System.Windows.Forms.RadioButton();
+            this.rdoEnglish = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(103, 78);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblEmployeeName
+            // 
+            this.lblEmployeeName.AutoSize = true;
+            this.lblEmployeeName.Location = new System.Drawing.Point(31, 93);
+            this.lblEmployeeName.Name = "lblEmployeeName";
+            this.lblEmployeeName.Size = new System.Drawing.Size(84, 13);
+            this.lblEmployeeName.TabIndex = 4;
+            this.lblEmployeeName.Text = "Employee \tName";
+            // 
+            // lblEmployeeId
+            // 
+            this.lblEmployeeId.AutoSize = true;
+            this.lblEmployeeId.Location = new System.Drawing.Point(31, 116);
+            this.lblEmployeeId.Name = "lblEmployeeId";
+            this.lblEmployeeId.Size = new System.Drawing.Size(64, 13);
+            this.lblEmployeeId.TabIndex = 5;
+            this.lblEmployeeId.Text = "Employee\tID";
+            // 
+            // lblHoursWorked
+            // 
+            this.lblHoursWorked.AutoSize = true;
+            this.lblHoursWorked.Location = new System.Drawing.Point(31, 163);
+            this.lblHoursWorked.Name = "lblHoursWorked";
+            this.lblHoursWorked.Size = new System.Drawing.Size(76, 13);
+            this.lblHoursWorked.TabIndex = 6;
+            this.lblHoursWorked.Text = "Hours Worked";
+            // 
+            // lblTotalSales
+            // 
+            this.lblTotalSales.AutoSize = true;
+            this.lblTotalSales.Location = new System.Drawing.Point(31, 189);
+            this.lblTotalSales.Name = "lblTotalSales";
+            this.lblTotalSales.Size = new System.Drawing.Size(60, 13);
+            this.lblTotalSales.TabIndex = 7;
+            this.lblTotalSales.Text = "Total Sales";
+            // 
+            // lblSalesBonus
+            // 
+            this.lblSalesBonus.AutoSize = true;
+            this.lblSalesBonus.Location = new System.Drawing.Point(31, 215);
+            this.lblSalesBonus.Name = "lblSalesBonus";
+            this.lblSalesBonus.Size = new System.Drawing.Size(66, 13);
+            this.lblSalesBonus.TabIndex = 8;
+            this.lblSalesBonus.Text = "Sales Bonus";
+            // 
+            // txtEmployeeName
+            // 
+            this.txtEmployeeName.Location = new System.Drawing.Point(130, 90);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.Size = new System.Drawing.Size(100, 20);
+            this.txtEmployeeName.TabIndex = 9;
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.Location = new System.Drawing.Point(130, 113);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(100, 20);
+            this.txtEmployeeID.TabIndex = 10;
+            // 
+            // txtHoursWorked
+            // 
+            this.txtHoursWorked.Location = new System.Drawing.Point(130, 160);
+            this.txtHoursWorked.Name = "txtHoursWorked";
+            this.txtHoursWorked.Size = new System.Drawing.Size(100, 20);
+            this.txtHoursWorked.TabIndex = 11;
+            // 
+            // txtTotalSales
+            // 
+            this.txtTotalSales.Location = new System.Drawing.Point(130, 186);
+            this.txtTotalSales.Name = "txtTotalSales";
+            this.txtTotalSales.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalSales.TabIndex = 12;
+            // 
+            // txtSalesBonus
+            // 
+            this.txtSalesBonus.Location = new System.Drawing.Point(130, 212);
+            this.txtSalesBonus.Name = "txtSalesBonus";
+            this.txtSalesBonus.ReadOnly = true;
+            this.txtSalesBonus.Size = new System.Drawing.Size(100, 20);
+            this.txtSalesBonus.TabIndex = 13;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(20, 259);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 14;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(113, 259);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 15;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(207, 259);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 16;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdoFrench);
+            this.groupBox1.Controls.Add(this.rdoEnglish);
+            this.groupBox1.Location = new System.Drawing.Point(130, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(103, 68);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Language";
+            // 
+            // rdoFrench
+            // 
+            this.rdoFrench.AutoSize = true;
+            this.rdoFrench.Location = new System.Drawing.Point(7, 37);
+            this.rdoFrench.Name = "rdoFrench";
+            this.rdoFrench.Size = new System.Drawing.Size(65, 17);
+            this.rdoFrench.TabIndex = 1;
+            this.rdoFrench.Text = "Français";
+            this.rdoFrench.UseVisualStyleBackColor = true;
+            this.rdoFrench.CheckedChanged += new System.EventHandler(this.rdoFrench_CheckedChanged);
+            // 
+            // rdoEnglish
+            // 
+            this.rdoEnglish.AutoSize = true;
+            this.rdoEnglish.Checked = true;
+            this.rdoEnglish.Location = new System.Drawing.Point(6, 19);
+            this.rdoEnglish.Name = "rdoEnglish";
+            this.rdoEnglish.Size = new System.Drawing.Size(59, 17);
+            this.rdoEnglish.TabIndex = 0;
+            this.rdoEnglish.TabStop = true;
+            this.rdoEnglish.Text = "English";
+            this.rdoEnglish.UseVisualStyleBackColor = true;
+            this.rdoEnglish.CheckedChanged += new System.EventHandler(this.rdoEnglish_CheckedChanged);
+            // 
+            // MailOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(294, 294);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtSalesBonus);
+            this.Controls.Add(this.txtTotalSales);
+            this.Controls.Add(this.txtHoursWorked);
+            this.Controls.Add(this.txtEmployeeID);
+            this.Controls.Add(this.txtEmployeeName);
+            this.Controls.Add(this.lblSalesBonus);
+            this.Controls.Add(this.lblTotalSales);
+            this.Controls.Add(this.lblHoursWorked);
+            this.Controls.Add(this.lblEmployeeId);
+            this.Controls.Add(this.lblEmployeeName);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MailOrder";
+            this.Text = "Mail Order";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblEmployeeName;
+        private System.Windows.Forms.Label lblEmployeeId;
+        private System.Windows.Forms.Label lblHoursWorked;
+        private System.Windows.Forms.Label lblTotalSales;
+        private System.Windows.Forms.Label lblSalesBonus;
+        private System.Windows.Forms.TextBox txtEmployeeName;
+        private System.Windows.Forms.TextBox txtEmployeeID;
+        private System.Windows.Forms.TextBox txtHoursWorked;
+        private System.Windows.Forms.TextBox txtTotalSales;
+        private System.Windows.Forms.TextBox txtSalesBonus;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdoEnglish;
+        private System.Windows.Forms.RadioButton rdoFrench;
     }
 }
 
--- MailOrder/Form1.cs
+++ MailOrder/Form1.cs
@@ -10,11 +10,74 @@ using System.Windows.Forms;
 
 namespace MailOrder
 {
-    public partial class Form1 : Form
+    public partial class MailOrder : Form
     {
-        public Form1()
+        public MailOrder()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double _hoursWorked = double.Parse(txtHoursWorked.Text);
+
+            double _percentageOfHoursWorked = _hoursWorked / 160;
+
+            double _monthlySales = double.Parse(txtTotalSales.Text);
+
+            double _totalBonusAmount = _monthlySales * 0.02;
+
+            double _salesBonus = _totalBonusAmount * _percentageOfHoursWorked;
+
+            txtSalesBonus.Text = Convert.ToString(_salesBonus);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdoFrench_CheckedChanged(object sender, EventArgs e)
+        {
+            lblEmployeeId.Text = "Employé ID";
+            lblEmployeeName.Text = "Nom de l'employé";
+            lblHoursWorked.Text = "Heures travaillées";
+            lblTotalSales.Text = "Ventes totales";
+            lblSalesBonus.Text = "Bonus de vente";
+            btnCalculate.Text = "Calculer";
+            btnPrint.Text = "Impression";
+            btnClear.Text = "Clair";
+            groupBox1.Text = "Langue";
+        }
+
+        private void rdoEnglish_CheckedChanged(object sender, EventArgs e)
+        {
+            lblEmployeeId.Text = "Employee ID";
+            lblEmployeeName.Text = "Employee Name";
+            lblHoursWorked.Text = "Hours Worked";
+            lblTotalSales.Text = "Total Sales";
+            lblSalesBonus.Text = "Sales Bonus";
+            btnCalculate.Text = "Calculate";
+            btnPrint.Text = "Print";
+            btnClear.Text = "Clear";
+            groupBox1.Text = "Language";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtEmployeeName.Clear();
+            txtEmployeeID.Clear();
+            txtHoursWorked.Clear();
+            txtSalesBonus.Clear();
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your form has been sent to the Printer", "Print",
+                                 MessageBoxButtons.OK,
+                                 MessageBoxIcon.Information);
+
         }
     }
 }
